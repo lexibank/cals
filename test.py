8e25c5b8967bee2d7b3afd9ff88d1b54a7fe9570
chrzyki <christoph@functor-argument.net>
2020-02-20 10:45:04 +0100

    Change remote to HTTPS, add build badge, add tests

--- test.py
+++ test.py
@@ -1,2 +1,20 @@
 def test_valid(cldf_dataset, cldf_logger):
     assert cldf_dataset.validate(log=cldf_logger)
+
+
+def test_forms(cldf_dataset):
+    assert len(list(cldf_dataset["FormTable"])) == 15742
+    assert any(f["Form"] == "sʉjøk" for f in cldf_dataset["FormTable"])
+
+
+def test_parameters(cldf_dataset):
+    assert len(list(cldf_dataset["ParameterTable"])) == 183
+
+
+def test_languages(cldf_dataset):
+    assert len(list(cldf_dataset["LanguageTable"])) == 88
+
+
+def test_cognates(cldf_dataset):
+    assert len(list(cldf_dataset["CognateTable"])) == 15742
+    assert any(f["Form"] == "qʊs" for f in cldf_dataset["CognateTable"])
